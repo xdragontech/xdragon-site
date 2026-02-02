beffe8968061b677b17273ace9a5964e05a64028
Grant Rosenthal <grant@omegasys.eu>
2026-02-02 01:35:48 -0800

    Add migration for login events and password reset

---
+++ prisma/migrations/20260202090215_add_login_events_and_password_reset/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "PasswordResetToken" (
+    "identifier" TEXT NOT NULL,
+    "token" TEXT NOT NULL,
+    "expires" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "PasswordResetToken_token_key" ON "PasswordResetToken"("token");
+
+-- CreateIndex
+CREATE INDEX "PasswordResetToken_identifier_idx" ON "PasswordResetToken"("identifier");
