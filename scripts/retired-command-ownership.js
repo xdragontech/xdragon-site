44e26c7e3722d74742c183e2f9464974abb340c5
Grant Rosenthal <grant@omegasys.eu>
2026-03-30 09:27:01 -0700

    Retire legacy website tooling

--- scripts/retired-command-ownership.js
+++ scripts/retired-command-ownership.js
@@ -1,8 +1,16 @@
 #!/usr/bin/env node
 
-const surface = process.argv[2] || "This operator surface";
+function failRetiredSurface(surface) {
+  console.error(
+    `[retired] ${surface || "This operator surface"} has been retired from xdragon-site. Use the equivalent tooling in the command repo instead.`
+  );
+  process.exit(1);
+}
 
-console.error(
-  `[retired] ${surface} has been retired from xdragon-site. Use the equivalent tooling in the command repo instead.`
-);
-process.exit(1);
+if (require.main === module) {
+  failRetiredSurface(process.argv[2]);
+}
+
+module.exports = {
+  failRetiredSurface,
+};
